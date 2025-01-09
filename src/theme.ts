3da0ed5ac4701bebee300804853dc89e964cbf2d
Maxson Santos <maxsonsantos88@gmail.com>
2025-01-09 15:47:55 -0300

    feat: update theme colors

--- src/theme.ts
+++ src/theme.ts
@@ -8,9 +8,12 @@ const theme = extendTheme({
   colors: {
     white: "#FFFFFF",
     black: "#171923",
-    zinc: "#4A5568",
+    slate: "#4A5568",
     blue: "#0069CA",
-    purple: "#8C19D2",
+    purple: {
+      500: "#8c19d2",
+      600: "#6a13a2",
+    },
   },
 });
 
