04a552ceaf0c27069815bc384e2be8441c1dd182
Maxson Santos <maxsonsantos88@gmail.com>
2025-01-09 15:49:22 -0300

    feat: enhance Home page with layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,43 @@
 import React from "react";
+import {
+  Flex,
+  Box,
+  Input,
+  Button,
+  Text,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { CiSearch } from "react-icons/ci";
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Flex justifyContent="center" alignItems="center" h="100vh">
+      <Flex direction="column" w="100%" maxW="800px" p={4} gap={12}>
+        <Flex justifyContent="center" alignItems="center" gap={6}>
+          <Text fontSize={{ base: 50, md: 80 }} textAlign={"center"}>
+            <Box as="span" color="blue">
+              Search
+            </Box>{" "}
+            <Box as="span" color="purple.500">
+              d_evs
+            </Box>
+          </Text>
+        </Flex>
+
+        <Flex gap={6}>
+          <InputGroup>
+            <InputLeftElement pointerEvents="none">
+              <CiSearch size={20} color="slate" />
+            </InputLeftElement>
+            <Input placeholder="Search" />
+          </InputGroup>
+          <Button colorScheme="purple" w={48}>
+            Search
+          </Button>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
 
