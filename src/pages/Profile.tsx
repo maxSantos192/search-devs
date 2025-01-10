757ecab24a6ebcc3b9cca8e27b70fe5fd0912328
Maxson Santos <maxsonsantos88@gmail.com>
2025-01-10 01:04:56 -0300

    feat: enhance Profile page with components

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Flex, VStack } from "@chakra-ui/react";
+import Header from "../components/Header";
+import CardProfile from "../components/CardProfile";
+import CardRepository from "../components/CardRepository";
 
 function Profile() {
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <VStack h={"100vh"} bg={"layout"}>
+      <Header />
+      <Flex gap={10} mt={10} px={5}>
+        <CardProfile />
+        <CardRepository />
+      </Flex>
+    </VStack>
   );
 }
-
 export default Profile;
